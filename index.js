4c008c562ac1109f6ef4e1a0b3de0458a24b6463
Caezar Olivares <caezar.olivares@crescendocollective.com>
2023-06-02 19:03:10 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: 'http://localhost:9019',
+		methods: ['GET', 'POST'],
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log(`User Connected: ${socket.id}`);
+
+	// socket.on('game-dev-webhook', (data) => {
+	// })
+});
+
+server.listen(4000, () => 'Server is running');
